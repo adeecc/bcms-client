764c70934bb9ce4540f32ddca2adbc1cf45ff600
adichopra11 <adi.chopra108@gmail.com>
2021-03-28 16:04:31 +0530

    Cleaned Up Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import { Link } from "react-router-dom";
 
 interface Props {}
 
 const Home: React.FC = (props: Props) => {
+  const logoStyes:CSSProperties = {
+    height: "1em",
+    width: "1em"
+  }
+
   return (
-    <div>
-      <h4 className="text-primary-100">What is typeScript, if not javaScript persevering!</h4>
-      <div className="mx-auto my-auto mh-screen flex justify-evenly">
+    <div className="container flex flex-col min-h-screen justify-evenly">
+      <h1 className="mx-auto text-primary-100">Welcome to <img className="inline-block" style={logoStyes} src="logo512.png" />
+      CMS</h1>
+      <div className="flex justify-evenly">
         <Link to="signup" className="flex-initial bg-accent text-button py-2 px-4 rounded shadow">Sign Up</Link>
         <Link to="signin" className="flex-initial bg-accent text-button py-2 px-4 rounded shadow">Sign In</Link>
       </div>
