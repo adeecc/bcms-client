50d11ab429e5f12457bebcaeea7499210ad51b18
adichopra11 <adi.chopra108@gmail.com>
2021-03-30 01:02:26 +0530

    Separate PostList and Feed

--- src/modules/Feed.tsx
+++ src/modules/Feed.tsx
@@ -1,47 +1,10 @@
-import React, { useEffect, useState } from "react";
-import PostCard from "../ui/PostCard";
-
-import postData from "./sample";
+import React from "react";
+import PostList from "../ui/PostList";
 
 interface Props {}
 
-interface Post {
-  id: string;
-  title: string;
-  courseName: string;
-  created_at: Date;
-  updated_at: Date;
-  tags?: string[];
-}
-
-const Feed: React.FC<Props> = (props: Props) => {
-  const [posts, setPosts] = useState<Post[]>(() =>
-    JSON.parse(postData, (key, value) => {
-      console.log(key);
-      if (key === "created_at" || key === "updated_at") return new Date(value);
-      else return value;
-    })
-  );
-
-  //   useEffect(() => {
-  //     // Get Posts from the API
-  //     // const loadedPosts = axiosClient.get();
-
-  //   }, [posts]);
-
-  return (
-    <div>
-      {posts.map((value, index) => (
-        <PostCard
-          id="{index}"
-          title={value.title}
-          courseName={value.courseName}
-          created_at={value.created_at}
-          updated_at={value.updated_at}
-        />
-      ))}
-    </div>
-  );
+const Feed: React.FC = ({}) => {
+  return <PostList maxPosts={5}/>;
 };
 
 export default Feed;
---
+++ src/ui/PostList.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import PostCard from "./PostCard";
+
+import postData from "../modules/sample";
+
+interface Props {
+  courseId?: number;
+  maxPosts?: number;
+}
+
+interface Post {
+  id: string;
+  title: string;
+  courseName: string;
+  created_at: Date;
+  updated_at: Date;
+  tags?: string[];
+}
+
+const PostList: React.FC<Props> = ({ courseId, maxPosts }) => {
+  const [posts, setPosts] = useState<Post[] | null>(null);
+
+  useEffect(() => {
+    // Get Posts from the API
+    // const loadedPosts = axiosClient.get(`{apiBaseUrl}/posts/courseId`);
+
+    const loadedPosts = JSON.parse(postData, (key, value) => {
+      if (key === "created_at" || key === "updated_at") return new Date(value);
+      else return value;
+    });
+
+    if (maxPosts) {
+      setPosts(loadedPosts.slice(0, maxPosts));
+    } else {
+      setPosts(loadedPosts);
+    }
+  }, [maxPosts, courseId]);
+
+  return (
+    <div>
+      {posts?.map((value, index) => (
+        <PostCard
+          id="{index}"
+          title={value.title}
+          courseName={value.courseName}
+          created_at={value.created_at}
+          updated_at={value.updated_at}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default PostList;
