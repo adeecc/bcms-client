196c1cbb3b532e4ee804aaa11e9c06072bf404c1
adichopra11 <adi.chopra108@gmail.com>
2021-03-30 23:08:36 +0530

    Use new Logo

--- src/ui/layout/LeftPanel.tsx
+++ src/ui/layout/LeftPanel.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import logo from "../../img/logo2.png";
+
 interface Props {
   gridStyle: string;
 }
@@ -10,12 +12,17 @@ const LeftPanel: React.FC<Props> = (props: Props) => {
     <div className={props.gridStyle}>
       <div className="pt-5 sticky top-0 lg:h-screen flex flex-col">
         {/* Logo */}
-        <div className="mb-7">
+        <div className="mb-7 pt-5">
           <div className="w-full flex justify-center text-primary-100">
             <Link to="/">
-              <h1>
-                <span className="text-accent">B</span>CMS
-              </h1>
+              <img
+                className="inline-block"
+                style={{
+                  height: "40px",
+                  // width: "40px",
+                }}
+                src={logo}
+              />
             </Link>
           </div>
         </div>
