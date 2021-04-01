eb07013a66aba589b7a95b727f3e368e5e02f23d
adichopra11 <adi.chopra108@gmail.com>
2021-04-02 02:57:31 +0530

    Added custom header to Courses page

--- src/modules/Courses.tsx
+++ src/modules/Courses.tsx
@@ -7,6 +7,12 @@ interface Props {}
 const Courses: React.FC<Props> = (props: Props) => {
   return (
     <div>
+      <div className="flex justify-between items-end mb-5">
+        <h3 className="text-primary-100 my-auto">Your Courses</h3>
+        <button className="py-2 px-6 my-auto rounded-lg text-button bg-accent">
+          Enroll
+        </button>
+      </div>
       <CourseList maxCourses={15} />
     </div>
   );
