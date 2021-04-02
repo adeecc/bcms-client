19e6e50670f5519fd73579acd84a4c35384ba61a
adichopra11 <adi.chopra108@gmail.com>
2021-04-02 18:51:07 +0530

    Create Tag Component

---
+++ src/ui/Tag.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Props {
+  label: string;
+}
+
+const Tag: React.FC<Props> = ({ label }) => {
+  return (
+    <div className="bg-primary-700 text-primary-200 rounded mx-1 px-2 py-1 shadow-sm">
+      {label}
+    </div>
+  );
+};
+
+export default Tag;
