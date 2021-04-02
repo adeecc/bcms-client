574c942d234c4e6c45047286cb6190c5bf82fbad
adichopra11 <adi.chopra108@gmail.com>
2021-04-03 02:22:00 +0530

    Fixed minor bug

--- src/ui/GlobalSearch.tsx
+++ src/ui/GlobalSearch.tsx
@@ -1,23 +1,21 @@
-import React from 'react'
+import React from "react";
 import SvgSearch from "../icons/SolidSearch";
 
-interface Props {
-    
-}
+interface Props {}
 
 const GlobalSearch: React.FC<Props> = (props: Props) => {
-    return (
-        <div className="flex items-center bg-primary-700 hover:bg-primary-600 text-primary-300 focus-within:text-primary-100 rounded-lg">
-        <div className="h-full mx-4">
-          <SvgSearch />
-        </div>
-        <input
-          type="text"
-          placeholder="Search for Courses, Users or Posts"
-          className="w-full py-3 px-4 rounded-8 bg-transparent placeholder-primary-300 focus:outline-none"
-        />
+  return (
+    <div className="w-full flex items-center bg-primary-700 hover:bg-primary-600 text-primary-300 focus-within:text-primary-100 rounded-lg">
+      <div className="h-full px-4">
+        <SvgSearch />
       </div>
-    )
-}
+      <input
+        type="text"
+        placeholder="Search for Courses, Users or Posts"
+        className="w-full py-3 px-4 rounded-8 bg-transparent placeholder-primary-300 focus:outline-none"
+      />
+    </div>
+  );
+};
 
-export default GlobalSearch
+export default GlobalSearch;
