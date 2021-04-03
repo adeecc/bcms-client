9061a8cbe4ed43d3501ec53b93697e79048f57d3
adichopra11 <adi.chopra108@gmail.com>
2021-04-03 20:08:18 +0530

    Use React-md-editor for Rich Text

--- src/modules/forms/PostCreateForm.tsx
+++ src/modules/forms/PostCreateForm.tsx
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
-import ReactQuill from "react-quill";
-
-import "react-quill/dist/quill.bubble.css";
-import "react-quill/dist/quill.snow.css";
+import MDEditor from "@uiw/react-md-editor";
 
 interface Props {
   courseId: number;
@@ -18,23 +15,6 @@ const PostCreateForm: React.FC<Props> = ({ courseId }) => {
 
   const [courseCode, setCourseCode] = useState<string>("");
   const [courseName, setCourseName] = useState<string>("");
-
-  const modules = {
-    toolbar: [
-      [{ header: [1, 2, false] }],
-      ["bold", "italic", "underline", "strike", "blockquote"],
-      [
-        { list: "ordered" },
-        { list: "bullet" },
-        { indent: "-1" },
-        { indent: "+1" },
-      ],
-      [{ script: "sub" }, { script: "super" }],
-      ["formula", "code-block"],
-      ["link"],
-      ["clean"],
-    ],
-  };
 
   const handleSubmit = (event: React.SyntheticEvent) => {
     event.preventDefault();
@@ -75,22 +55,23 @@ const PostCreateForm: React.FC<Props> = ({ courseId }) => {
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             required
-            className="w-full bg-primary-700 hover:bg-primary-600 text-primary-100 border border-primary-600 px-3 py-2 mt-1 rounded-lg shadow-sm focus:outline-none focus:border-accent focus:ring-1 focus:ring-accent"
+            className="w-full bg-primary-100 hover:bg-primary-100 text-primary-100 border border-primary-600 px-3 py-2 mt-1 rounded-lg shadow-sm focus:outline-none focus:border-accent focus:ring-1 focus:ring-accent"
           />
         </div>
         <div className="field-group">
           <label htmlFor="body" className="font-medium text-primary-100">
             Body
           </label>
-          <ReactQuill
-            theme="bubble"
+          <MDEditor
             value={body}
-            placeholder=""
-            onChange={setBody}
-            modules={modules}
-            // formats={formats}
-            bounds="form"
-            className="w-full bg-primary-700 hover:bg-primary-600 text-primary-100 border border-primary-600 px-3 py-2 mt-1 rounded-lg shadow-sm focus:outline-none focus:border-accent focus:ring-1 focus:ring-accent"
+            onChange={(value: string | undefined) => {
+              value && setBody(value);
+            }}
+            preview="edit"
+            minHeight={200}
+            hideToolbar
+            highlightEnable
+            className="w-full bg-primary-100 hover:bg-primary-100 border border-primary-600 mt-1 rounded-lg shadow-sm focus:outline-none focus:border-accent focus:ring-1 focus:ring-accent"
           />
         </div>
         <div className="field-group">
@@ -104,7 +85,7 @@ const PostCreateForm: React.FC<Props> = ({ courseId }) => {
             value={tags}
             onChange={(e) => setTags(e.target.value)}
             required
-            className="w-full bg-primary-700 hover:bg-primary-600 text-primary-100 border border-primary-600 px-3 py-2 mt-1 rounded-lg shadow-sm focus:outline-none focus:border-accent focus:ring-1 focus:ring-accent"
+            className="w-full bg-primary-100 hover:bg-primary-100 text-primary-100 border border-primary-600 px-3 py-2 mt-1 rounded-lg shadow-sm focus:outline-none focus:border-accent focus:ring-1 focus:ring-accent"
           />
         </div>
         <div className="field-group col-span-1">
--- src/ui/PostDetail.tsx
+++ src/ui/PostDetail.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import MDEditor from '@uiw/react-md-editor';
 
 import ReactQuill from "react-quill";
 
@@ -18,8 +19,7 @@ const PostDetail: React.FC<Props> = ({}) => {
     return {
       id,
       title: "Que Sara Sara",
-      body:
-        "<h2>Nigga what teh fuck</h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi voluptas earum blanditiis, voluptate id ab minima natus error consequatur, quia, accusantium consectetur architecto ea doloremque tempora. Corporis quisquam incidunt mollitia beatae? Iusto hic exercitationem, aliquam veniam quam expedita est porro? Possimus repudiandae, odit praesentium labore nisi libero nemo. <p> <br><br> Blanditiis, laborum magni vero qui sunt aut tempore deserunt incidunt, quidem quia cupiditate impedit, ipsam reprehenderit optio ad. Architecto molestiae qui ratione deleniti libero, dicta laborum, magnam reiciendis aliquid aut assumenda fuga aperiam amet iste dolore nostrum praesentium molestias ipsa, pariatur fugiat provident quo. Deleniti minima, sunt quas quibusdam cumque voluptatem dolor accusantium tempora eos harum aspernatur, ut esse expedita beatae dolores qui? Ducimus accusantium perferendis asperiores quas eius? Architecto aut earum cum magni laudantium exercitationem. Quo quod voluptatum reprehenderit necessitatibus cupiditate corporis nisi, rem praesentium cum, ut expedita unde voluptate commodi ducimus, ab eum possimus. Iure ratione, nostrum cum distinctio animi temporibus voluptatem aspernatur quaerat ab dolores facilis possimus? Excepturi aspernatur beatae nemo? Beatae labore amet totam qui dolores! Vitae, consequuntur doloribus magni, hic fugiat explicabo similique, aliquam ducimus saepe laboriosam voluptatem! Vitae aliquam similique tempora dicta, temporibus odio rem magni libero itaque, earum velit? Accusantium illum unde blanditiis cum nam aliquid error quos dolore temporibus! Numquam, modi nisi sed quo aliquam doloremque ipsam cupiditate. Repellat reprehenderit officia inventore, corrupti voluptas tempore, magnam corporis, impedit quam nesciunt natus reiciendis. Debitis eos corrupti beatae blanditiis modi tempora, obcaecati at fugit molestias animi quod odio pariatur maxime. Sequi, laudantium eum! Culpa ipsa beatae eius aspernatur laudantium recusandae alias repudiandae aliquid dignissimos molestias incidunt mollitia amet deleniti est assumenda, laboriosam quibusdam iusto, odio harum quia tenetur debitis quos vel? Aliquid incidunt, voluptates beatae commodi mollitia minus sunt soluta velit cum, quaerat nostrum sapiente quidem esse placeat sequi. Pariatur sequi doloribus eos rem a esse cupiditate? Odit, laborum obcaecati aspernatur delectus voluptates velit totam! Amet, sequi et non rem repudiandae iusto, quibusdam, fuga ab officiis ratione quis eos nihil fugiat molestiae perspiciatis maiores ut? Possimus ullam ratione quas tempore doloremque ducimus repudiandae laudantium, eius dignissimos iure aliquam voluptatem facilis commodi ut, velit itaque totam blanditiis voluptate quos harum atque. Distinctio non odio minus maxime odit, perspiciatis ipsa vitae dignissimos commodi nulla sapiente sed consequuntur eligendi iste eum quasi laboriosam, quos omnis tenetur accusamus veniam modi. Earum magnam quis, expedita laborum velit vel temporibus ipsam exercitationem laudantium? Et quaerat possimus incidunt dolorum repellendus, sed hic ad necessitatibus aliquam atque non voluptas saepe totam molestias aspernatur accusantium ab cum fugiat doloremque! Consequuntur, pariatur eos! Quibusdam minima odit, quas quis ipsum consequuntur officiis, quam accusantium inventore atque nostrum, est dignissimos neque. Soluta, rerum dolores iste doloremque ipsum expedita beatae impedit. Adipisci molestias aspernatur illo id iusto quae soluta iure sint alias placeat officia vel itaque odio ullam omnis animi, cumque libero! Cupiditate corrupti sunt rem, nemo tempora natus possimus magni reiciendis libero officia tenetur adipisci cum quaerat non modi ea ullam animi aut dicta nisi. Tempore, eius nostrum reprehenderit rerum, totam laborum cumque molestias sit quaerat, est facilis?",
+      body:`# Headering 1\n> Okauy this is very gg as well ngl\nhello | darkness\n--- ---\nmy old | friend`,
       courseName: "Lorem ipsum dolor sit",
       created_at: new Date(
         "Thu Apr 01 2021 17:56:34 GMT+0530 (India Standard Time)"
@@ -77,12 +77,7 @@ const PostDetail: React.FC<Props> = ({}) => {
           })}
         </div>
       </div>
-      <ReactQuill
-        theme="bubble"
-        value={post.body}
-        readOnly
-        className="w-full bg-primary-900 hover:bg-primary-900 text-primary-100 mt-5 focus:outline-none"
-      />
+      <MDEditor.Markdown source={post.body} className="w-full bg-primary-900 text-primary-100 mt-5 focus:outline-none"/>
     </div>
   );
 };
