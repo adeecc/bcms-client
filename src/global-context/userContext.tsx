d700b4c781760f09c7cda82746d5474580c8e376
adichopra11 <adi.chopra108@gmail.com>
2021-04-08 02:34:18 +0530

    Remove Wrong Comments

--- src/global-context/userContext.tsx
+++ src/global-context/userContext.tsx
@@ -50,28 +50,3 @@ const UserProvider: React.FC = ({ children }) => {
 };
 
 export { UserContext, UserProvider };
-
-// TO use:
-
-// const { state, dispatch } = React.useContext(UserContext);
-
-// const logIn = () => {
-//   dispatch({
-//     type: ActionTypes.LogIn,
-//     payload: {
-//       isLoggedIn: true,
-//       id: 1234,
-//       username: "adichopra11",
-//       fullName: "Aditya Chopra",
-//     },
-//   });
-// };
-
-// const logOut = () => {
-//   dispatch({
-//     type: ActionTypes.LogOut,
-//     payload: {
-//       isLoggedIn: false,
-//     },
-//   });
-// };
