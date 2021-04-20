38ee9ab0271ca3b4e860df2facd5cc84f6727ccb
adichopra11 <adi.chopra108@gmail.com>
2021-04-20 18:21:52 +0530

    Minor Changes in Reports

--- src/components/reports/Reports.tsx
+++ src/components/reports/Reports.tsx
@@ -1,4 +1,4 @@
-import { DownloadIcon } from "@heroicons/react/solid";
+import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -12,7 +12,7 @@ const ReportCard: React.FC<ReportCardProps> = ({ title, url }) => {
     <div className="w-full bg-primary-800 p-6 rounded-lg flex justify-between">
       <h5 className="text-primary-100 font-bold truncate  my-auto">{title}</h5>
       <Link to={url} className="my-auto">
-        <DownloadIcon className="w-6 text-accent" />
+        <ArrowRightIcon className="w-6 text-accent" />
       </Link>
     </div>
   );
@@ -31,18 +31,13 @@ const Reports: React.FC<ReportsProps> = () => {
       title: "Course Statistics and Information",
       url: "/reports/courses",
     },
-
-    {
-      title: "Post Statistics and Information",
-      url: "",
-    },
   ];
   return (
     <div>
       <div className="flex justify-between items-end mb-5 ">
         <h3 className="text-primary-100 my-auto">Available Reports</h3>
         <button className="py-2 px-6 my-auto rounded-lg text-button bg-accent outline-none focus:outline-none">
-          <DownloadIcon className="w-5" />
+          <ArrowRightIcon className="w-5" />
         </button>
       </div>
       <div className="flex flex-col space-y-7 my-7 ">
